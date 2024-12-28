1ee4b77dc4779b09a21e4867b327442d0f1a5d32
fatma mohamed <fatmamohamed2101@gmail.com>
2024-12-28 11:49:58 +0200

    auth

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,12 @@ Route::prefix('admin')->group(function() {
 Route::middleware('auth:admin')->group(function () {
     Route::post('certificates', [CertificateController::class, 'store']);
     Route::get('all-certificates', [CertificateController::class, 'index']);
-    Route::get('certificates/{national_id}', [CertificateController::class, 'show']);
     Route::get('certificate/{id}', [CertificateController::class, 'getbyid']);
-    
     Route::delete('certificates/{id}', [CertificateController::class, 'destroy']);
     Route::delete('certificates', [CertificateController::class, 'bulkDelete']);
     Route::put('certificates/{id}', [CertificateController::class, 'update']);
     
-    
-
 });
+
+Route::get('certificates/{national_id}', [CertificateController::class, 'show']);
+
